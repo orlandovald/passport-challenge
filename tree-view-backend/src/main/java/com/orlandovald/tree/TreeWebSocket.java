8454444a8caf060672d95037e8503b9d18e320db
Orlando Valdez <orlandovald@gmail.com>
2019-10-01 18:28:26 -0400

    Add websocket logging

--- tree-view-backend/src/main/java/com/orlandovald/tree/TreeWebSocket.java
+++ tree-view-backend/src/main/java/com/orlandovald/tree/TreeWebSocket.java
@@ -7,6 +7,10 @@ import io.micronaut.websocket.annotation.OnMessage;
 import io.micronaut.websocket.annotation.OnOpen;
 import io.micronaut.websocket.annotation.ServerWebSocket;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * WebSocket handler
@@ -14,19 +18,25 @@ import org.reactivestreams.Publisher;
 @ServerWebSocket("/ws/tree")
 public class TreeWebSocket {
 
-    private final WebSocketBroadcaster broadcaster;
+    private static final Logger log = LoggerFactory.getLogger(TreeWebSocket.class);
 
-    public TreeWebSocket(WebSocketBroadcaster broadcaster) {
+    private final WebSocketBroadcaster broadcaster;
+    private final TreeService service;
+
+    public TreeWebSocket(WebSocketBroadcaster broadcaster, TreeService service) {
         this.broadcaster = broadcaster;
+        this.service = service;
     }
 
     @OnOpen
-    public Publisher<String> onOpen(WebSocketSession session) {
-        return broadcaster.broadcast("Hola!");
+    public Publisher<List<Node>> onOpen(WebSocketSession session) {
+        log.info("Opening session: " + session.getId());
+        return broadcaster.broadcast(service.getFullTree());
     }
 
     @OnMessage
     public Publisher<String> onMessage(String message, WebSocketSession session) {
+        log.info("Message received from session: " + session.getId() + " Message: " + message);
         String msg = "Message: " + message;
         return broadcaster.broadcast(msg);
     }
@@ -34,7 +44,7 @@ public class TreeWebSocket {
     @OnClose
     public Publisher<String> onClose(WebSocketSession session) {
         String msg = "Adios!";
-        System.out.println(msg);
+        log.info("Closing session: " + session.getId());
         return broadcaster.broadcast(msg);
     }
 
