05b51281a9d23de81f12c57af058456896f9c06c
Orlando Valdez <orlandovald@gmail.com>
2019-10-02 16:47:19 -0400

    Fix and enhanced generate numbers unit test

--- tree-view-backend/src/test/java/com/orlandovald/tree/TreeServiceTest.java
+++ tree-view-backend/src/test/java/com/orlandovald/tree/TreeServiceTest.java
@@ -1,5 +1,6 @@
 package com.orlandovald.tree;
 
+import com.orlandovald.tree.pojo.TreeException;
 import io.micronaut.test.annotation.MicronautTest;
 import io.micronaut.test.annotation.MockBean;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import javax.inject.Inject;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.mock;
 
 @MicronautTest
@@ -22,11 +24,31 @@ class TreeServiceTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"-1,0,1,10", "0,0,1,10", "1,1,1,10", "15,15,0,10", "8,8,45,423", "13,13,462542,1246573521"})
+    @CsvSource({"0,0,1,10", "1,1,1,10", "15,15,0,10", "8,8,45,423", "13,13,462542,1246573521"})
     void generateRandomNumbers(int count, int expected, int min, int max) {
 assertThat(service.generateRandomNumbers(count,min,max))
         .hasSize(expected)
         .allMatch(num -> num >= min && num <= max);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-1,0,1,10", "16,0,1,10"})
+    void generateRandomNumbersThrowCountExceptions(int count, int expected, int min, int max) {
+        assertThatExceptionOfType(TreeException.class).isThrownBy(() -> {
+            service.generateRandomNumbers(count,min,max);
+        })
+                .withMessageStartingWith("Child count should be between 0 and")
+                .withNoCause();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,10,100,5"})
+    void generateRandomNumbersThrowBoundEx(int count, int expected, int min, int max) {
+        assertThatExceptionOfType(TreeException.class).isThrownBy(() -> {
+            service.generateRandomNumbers(count,min,max);
+        })
+                .withMessage("Upper bound should be greater than lower bound")
+                .withNoCause();
     }
 
     @MockBean(TreeRepository.class)
