272e67cbec958a55ce7ee27e839e28e4346c9edf
Orlando Valdez <orlandovald@gmail.com>
2019-10-02 21:00:56 -0400

    Initial commit of front-end app

---
+++ tree-view-frontend/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <tree-view :treeNodes="treeNodes" />
+  </div>
+</template>
+
+<script>
+import TreeView from '@/components/TreeView.vue'
+export default {
+  name: "app",
+  components: {
+    TreeView,
+  },
+  data() {
+    return {
+      treeNodes: [
+        {
+          id: 1,
+          name: 'Orlando',
+          childs: [202539349, 107924618, 210693730],
+          lower_bound: 1,
+          upper_bound: 256432534
+        },
+        {
+          id: 2,
+          name: 'Second parent',
+          childs: [156, 524, 280, 922, 790],
+          lower_bound: 100,
+          upper_bound: 1000
+        }
+      ],
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+
+</style>
---
+++ tree-view-frontend/src/components/TreeView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="tree-view">
+    <h5>Root</h5>
+    <ul class="tree">
+      <li v-for="treeNode in treeNodes" :key="treeNode.id">{{treeNode.name}} 
+          <span class="new badge" data-badge-caption="range">{{ treeNode.lower_bound }} / {{ treeNode.upper_bound }}</span>
+        <ul v-if = "treeNode.childs!=null">
+          <li v-for="num in treeNode.childs" :key="num" style="font-weight:normal">{{ num }}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: 'tree-view',
+    props: {
+        treeNodes: Array,
+    },
+    }
+</script>
+
+<style scoped>
+    .item {
+    cursor: pointer;
+    }
+    .bold {
+    font-weight: bold;
+    }
+
+    ul.tree, ul.tree ul {
+    list-style: none;
+    margin: 10px;
+    padding: 0;
+    }
+
+    ul.tree ul {
+    margin-left: 20px;
+    }
+
+    ul.tree li {
+    margin: 0;
+    padding: 0 7px;
+    line-height: 20px;
+    color: #369;
+    font-weight: bold;
+    border-left: 1px dotted rgb(100, 100, 100);
+    }
+
+    ul.tree li:last-child {
+    border-left: none;
+    }
+
+    ul.tree li:before {
+    position: relative;
+    top: -0.3em;
+    height: 1em;
+    width: 12px;
+    color: white;
+    border-bottom: 1px dotted rgb(100, 100, 100);
+    content: '';
+    display: inline-block;
+    left: -7px;
+    }
+
+    ul.tree li:last-child:before {
+    border-left: 1px dotted rgb(100, 100, 100);
+    }
+</style>
