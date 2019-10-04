9331be5c939679836d0ea5cff4a44c4c23f4eb54
Orlando Valdez <orlandovald@gmail.com>
2019-10-03 22:33:09 -0400

    Sort nodes retrieval by created date in descending order

--- tree-view-backend/src/main/java/com/orlandovald/tree/TreeRepository.java
+++ tree-view-backend/src/main/java/com/orlandovald/tree/TreeRepository.java
@@ -9,7 +9,6 @@ import io.reactiverse.reactivex.pgclient.Tuple;
 
 import javax.inject.Singleton;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,7 +20,7 @@ public class TreeRepository {
     public static final String SQL_INSERT_NODE = "INSERT INTO tree_nodes (name, lower_bound, upper_bound, childs) " +
             "VALUES ($1, $2, $3, $4) RETURNING id";
     public static final String SQL_SELECT_ALL_NODES = "SELECT id, name, lower_bound, upper_bound, childs, created_at " +
-            "FROM tree_nodes";
+            "FROM tree_nodes ORDER BY created_at DESC";
     public static final String SQL_DELETE_NODE = "DELETE FROM tree_nodes WHERE id = $1 RETURNING *";
     public static final String SQL_DELETE_CHILD = "UPDATE tree_nodes SET childs = array_remove(childs, $1) WHERE id = $2 RETURNING *";
     public static final String SQL_SELECT_NODE = "SELECT id, name, lower_bound, upper_bound, childs, created_at FROM tree_nodes WHERE id = $1";
